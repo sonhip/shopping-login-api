c758698334a07d1a7f75d5709b864e6db7494ff2
nghiemduong2000 <nghiemduong2000@gmail.com>
2021-04-27 16:05:19 +0700

    update whitelist origin cors

--- api.js
+++ api.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const sessionMiddleware = require('./middlewares/session');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const sessionMiddleware = require("./middlewares/session");
 dotenv.config();
 
 const app = express();
+
+const whitelist = [
+  "http://localhost:3000",
+  "https://petshop-mern-client-4e2bf.web.app",
+];
+
 app.use(
   cors({
-    origin: 'https://petshop-mern-client-4e2bf.web.app',
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
@@ -28,16 +40,16 @@ mongoose.connect(db, {
 });
 
 const { connection } = mongoose;
-connection.once('open', () => {
-  console.log('Mongo database connection established successfully');
+connection.once("open", () => {
+  console.log("Mongo database connection established successfully");
 });
 
-app.use('/api/products', sessionMiddleware, require('./routes/products'));
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/admin', require('./routes/admin'));
-app.use('/api/orders', require('./routes/orders'));
-app.use('/api/session', require('./routes/session'));
+app.use("/api/products", sessionMiddleware, require("./routes/products"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/admin", require("./routes/admin"));
+app.use("/api/orders", require("./routes/orders"));
+app.use("/api/session", require("./routes/session"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
